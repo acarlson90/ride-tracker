8610e6ccaa64ab7eb1fd081c9157f74b79c1b821
Aaron Carlson <aaron.carlson@ACCUSEDETECTTHE.workdayinternal.com>
2020-05-12 21:34:05 -0700

    Initial Commit

---
+++ src/main/java/com/aaroncarlson/RideTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.aaroncarlson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideTrackerApplication.class, args);
+	}
+
+}
