3346dbaf57e5bc847f38c1e699ca60a683299547
Aaron Carlson <aaron.carlson@ACCUSEDETECTTHE.workdayinternal.com>
2020-05-17 16:11:10 -0700

    Implementing JDBCTemplate

--- src/main/java/com/aaroncarlson/RideTrackerApplication.java
+++ src/main/java/com/aaroncarlson/RideTrackerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * NOTE: Both Transactional and Exception Handling is NOT enabled (the based components are exposed but no implemented)
+ */
 @SpringBootApplication
 public class RideTrackerApplication extends SpringBootServletInitializer {
 
--- src/main/java/com/aaroncarlson/model/Ride.java
+++ src/main/java/com/aaroncarlson/model/Ride.java
@@ -1,11 +1,14 @@
 package com.aaroncarlson.model;
 
 
+import java.sql.Timestamp;
+
 public class Ride {
 
     private Long id;
     private String name;
     private int duration;
+    private Timestamp timestamp;
 
     public Long getId() {
         return id;
@@ -29,5 +32,23 @@ public class Ride {
 
     public void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Ride{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", duration=" + duration +
+                ", timestamp=" + timestamp +
+                '}';
     }
 }
